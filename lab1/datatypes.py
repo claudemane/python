b8f22d708bc7006f0a27fa2cd55d3bb6ad937181
claudemane <157001560+claudemane@users.noreply.github.com>
2024-01-25 00:18:29 +0600

    Add files via upload

---
+++ lab1/datatypes.py
@@ -0,0 +1,32 @@
+x = 5 #int
+x = "Hello World"	#str	
+x = 20	#int	
+x = 20.5	#float	
+x = 1j	#complex	
+x = ["apple", "banana", "cherry"]	#list	
+x = ("apple", "banana", "cherry")	#tuple	
+x = range(6)	#range	
+x = {"name" : "John", "age" : 36}	#dict	
+x = {"apple", "banana", "cherry"}	#set	
+x = frozenset({"apple", "banana", "cherry"})	#frozenset	
+x = True	#bool	
+x = b"Hello"	#bytes	
+x = bytearray(5)	#bytearray	
+x = memoryview(bytes(5))	#memoryview	
+x = None
+print(type(x))
+
+x = str("Hello World")		
+x = int(20)		
+x = float(20.5)		
+x = complex(1j)		
+x = list(("apple", "banana", "cherry"))		
+x = tuple(("apple", "banana", "cherry"))		
+x = range(6)		
+x = dict(name="John", age=36)		
+x = set(("apple", "banana", "cherry"))		
+x = frozenset(("apple", "banana", "cherry"))		
+x = bool(5)		
+x = bytes(5)		
+x = bytearray(5)		
+x = memoryview(bytes(5))
