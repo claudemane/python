be46feb59bf229d71541fb13cbec349f2b123b64
claudemane <157001560+claudemane@users.noreply.github.com>
2024-03-07 14:20:21 +0500

    Create builtin_f5.py

---
+++ lab6/builtin_f5.py
@@ -0,0 +1,5 @@
+tuple = eval(input("Enter a tuple of boolean values:"))
+if all(tuple):
+    print("All elements in the tuple are True.")
+else:
+    print("Not all elements in the tuple are True.")
